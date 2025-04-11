aa0fefc0c2849639401aeed83ed093571fd615ac
MadupuHiranya07 <madupuhiranya07@gmail.com>
2025-04-11 10:35:29 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "20%" }}>
+    <div style={{ textAlign: "center" }}>
       <h1>Hello World!</h1>
     </div>
   );
